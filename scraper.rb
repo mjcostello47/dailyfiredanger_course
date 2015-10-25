ab578aab8007eaa3c7a09d89208f1f5d2c23bdfd
mjcostello47 <mcostello@me.com>
2015-10-25 13:23:45 +1100

    starting to get xml

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,16 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("http://www.rfs.nsw.gov.au/feeds/fdrToban.xml")
 #
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+p page.at('district.name')
 #
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
